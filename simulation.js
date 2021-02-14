5d12f2a1c13819d0c55ca2273f5d2c963d5d1c9e
dbankova <denbankova@gmail.com>
2021-02-14 12:58:34 +0000

    Deleted unused code

--- simulation.js
+++ simulation.js
@@ -1,11 +1,3 @@
-/*const initialAnimation = document.querySelector(".initialAnimation");
-const fullImage = document.querySelector(".fullImage");
-
-const tl = new TimelineMax();
-
-tl.fromTo(fullImage, 1, { height: "0%" }, { height: "80%" });
-*/
-
 function arrange() {
     var molecules = [document.getElementById("mol1"), document.getElementById("mol2"), document.getElementById("mol3"), document.getElementById("mol4"), document.getElementById("mol5"), document.getElementById("mol6"), document.getElementById("mol7"), document.getElementById("mol8"), document.getElementById("mol9"), document.getElementById("mol10"), document.getElementById("mol11"), document.getElementById("mol12"), document.getElementById("mol13"), document.getElementById("mol14"), document.getElementById("mol15")];
 
@@ -39,31 +31,4 @@ function arrange() {
     molecules[13].style.left = "350px";
     molecules[14].style.top = "320px";
     molecules[14].style.left = "500px";
-    /*var i;
-    var pos = 200;
-    for (i = 0; i < 15; i++) {
-        molecules[i].style.top = pos + "px";
-        molecules[i].style.left = pos + "200px";
-        pos+=10;
-    }*/
-    //mol5.style.top = "200px";
-    //mol5.style.left = "500px";
-
-    /*var pos = 200;
-    var id = setInterval(frame, 10);
-    function frame() {
-        if (pos == 350) {
-            clearInterval(id);
-        } else {
-            pos++;
-            molecules[0].style.top = pos + 'px';
-            molecules[0].style.left = pos + 'px';
-            molecules[1].style.top = pos + 'px';
-            molecules[1].style.left = pos - 'px';
-            //mol3.style.top = pos - 'px';
-            //mol3.style.left = pos + 'px';
-            //mol4.style.top = pos - 'px';
-            //mol4.style.left = pos - 'px';
-        }
-    }*/
 }
